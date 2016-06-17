2842aee4336c25b32eb7cecc11d1d5a253be47d4
lilw <lilw@ctrip.com>
2016-06-17 18:32:26 +0800

    ff

---
+++ luceneDemo-basic/src/main/java/com/lclz/search/BooleanQueryExample.java
@@ -0,0 +1,130 @@
+package com.lclz.search;
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.IntField;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
+public class BooleanQueryExample {
+
+	private Analyzer analyzer;
+	private static Directory directory;
+	private IndexWriter indexWriter;
+
+	public BooleanQueryExample() {
+		analyzer = new StandardAnalyzer();
+		directory = new RAMDirectory();
+		IndexWriterConfig config = new IndexWriterConfig(Version.LATEST,
+				analyzer);
+		try {
+			indexWriter = new IndexWriter(directory, config);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void addDocument() throws IOException {
+		Document doc = new Document();
+		StringField stringField = new StringField("name", "", Field.Store.YES);
+		TextField textField = new TextField("content", "", Field.Store.YES);
+		IntField intField = new IntField("num", 0, Field.Store.YES);
+
+		doc.removeField("name");
+		doc.removeField("content");
+		doc.removeField("num");
+		stringField.setStringValue("First");
+		textField.setStringValue("Humpty Dumpty sat on a wall,");
+		intField.setIntValue(100);
+		doc.add(stringField);
+		doc.add(textField);
+		doc.add(intField);
+		indexWriter.addDocument(doc);
+
+		doc.removeField("name");
+		doc.removeField("content");
+		doc.removeField("num");
+		stringField.setStringValue("Second");
+		textField.setStringValue("Humpty Dumpty had a great fall.");
+		intField.setIntValue(200);
+		doc.add(stringField);
+		doc.add(textField);
+		doc.add(intField);
+		indexWriter.addDocument(doc);
+
+		doc.removeField("name");
+		doc.removeField("content");
+		doc.removeField("num");
+		stringField.setStringValue("Third");
+		textField.setStringValue("All the king's horses and all the king's men");
+		intField.setIntValue(300);
+		doc.add(stringField);
+		doc.add(textField);
+		doc.add(intField);
+		indexWriter.addDocument(doc);
+
+		doc.removeField("name");
+		doc.removeField("content");
+		doc.removeField("num");
+		stringField.setStringValue("Fourth");
+		textField.setStringValue("Couldn't put Humpty together again.");
+		intField.setIntValue(400);
+		doc.add(stringField);
+		doc.add(textField);
+		doc.add(intField);
+		indexWriter.addDocument(doc);
+
+		indexWriter.commit();
+		indexWriter.close();
+
+	}
+
+	/**
+	 * BooleanClause.Occur.MUST 必须包含
+	 * BooleanClause.Occur.SHOULD  可能包含
+	 * BooleanClause.Occur.MUST_NOT 不能包含
+	 * @throws IOException
+	 */
+	public void booleanQuery() throws IOException {
+		System.out.println("booleanQuery========");
+		IndexReader reader = DirectoryReader.open(directory);
+		IndexSearcher searcher = new IndexSearcher(reader);
+
+		BooleanQuery query = new BooleanQuery();
+		query.add(new BooleanClause(new TermQuery(new Term("content", "humpty")), BooleanClause.Occur.MUST));
+		query.add(new BooleanClause(new TermQuery(new Term("content", "dumpty")), BooleanClause.Occur.MUST));
+		query.add(new BooleanClause(new TermQuery(new Term("content", "wall")), BooleanClause.Occur.SHOULD));
+		query.add(new BooleanClause(new TermQuery(new Term("content", "sat")), BooleanClause.Occur.MUST_NOT));
+
+		TopDocs topDocs = searcher.search(query, 100);
+
+		for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+			Document doc = reader.document(scoreDoc.doc);
+			System.out.println(doc.getField("name").stringValue());
+		}
+	}
+
+	public static void main(String args[]) throws IOException {
+		BooleanQueryExample ex = new BooleanQueryExample();
+		ex.addDocument();
+		ex.booleanQuery();
+	}
+}
